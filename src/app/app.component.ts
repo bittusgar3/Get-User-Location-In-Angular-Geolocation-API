0187e1533ec6319760c1dc9faf863c8963d33f13
Sagar Debnath <bittusagar3@noreply.github.com>
2023-04-18 15:53:45 +0530

    updateuserLocation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonService } from './service/common.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'location';
+  location:any;
+  locationjs: any;
+
+  constructor(private commanService:CommonService){}
+
+
+  ngOnInit(){
+    this.commanService.getLocation().subscribe((Response)=>{
+      console.log(Response);
+      this.location= Response;
+    });
+
+
+  navigator.geolocation.getCurrentPosition((position)=>{
+    console.log(position);
+    this.locationjs = position.coords;
+  });
+
+  }
 }
